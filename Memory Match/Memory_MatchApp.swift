50698c0923b59be9c9c81b0ed338b606b87e1c86
brendanbax <brendanbax@gmail.com>
2024-02-15 22:09:05 -0600

    Initial Commit

---
+++ Memory Match/Memory_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_MatchApp.swift
+//  Memory Match
+//
+//  Created by Brendan on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
