058ee7e345428fb96ea8d5dae35864c25a21713b
brendanbax <brendanbax@gmail.com>
2024-02-17 16:09:33 -0600

    Init project

--- Memory Match/ContentView.swift
+++ Memory Match/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
+func startGame() {
+    print("Starting game")
+}
+
 struct ContentView: View {
+    let gameThemeOptions = ["default", "animals", "food", "elements"]
+    let rows = [GridItem](repeating: GridItem(.fixed(75)), count: 4)
+
+    @State private var selectedTheme: String = ""
+    @State private var sessionDuration: Int = 0
+    @State private var score: Int = 0
+    @State private var activePiece: String = ""
+
+    var gameBoard: [GamePiece] {
+        generatePieces(theme: selectedTheme)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Game Clock")
+            Spacer()
+            LazyHGrid(rows: rows, spacing: 10, content: {
+                ForEach(gameBoard, id: \.id) { piece in
+                    BoardItem(piece: piece)
+                }
+            })
+            Spacer()
+            HStack {
+                Text("Theme:")
+                Picker("Select a theme", selection: $selectedTheme) {
+                                ForEach(gameThemeOptions, id: \.self) {
+                                    Text($0)
+                                }
+                            }
+                            .pickerStyle(.menu)
+            }
+            Spacer()
+            Button("Start", systemImage: "play", action: startGame)
+            Spacer()
         }
-        .padding()
+    }
+}
+
+struct BoardItem: View {
+    var piece: GamePiece
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .stroke(.blue, lineWidth: 3)
+                .aspectRatio(contentMode: .fit)
+            Text(piece.symbol)
+                .font(.largeTitle)
+                .padding()
+                .cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
