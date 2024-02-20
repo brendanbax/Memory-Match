e56d6bb434dea2fd3e4c23bb2aec3a8cf17b4c81
brendanbax <brendanbax@gmail.com>
2024-02-19 21:44:12 -0600

    Abstract enums to file

---
+++ Memory Match/Enums.swift
@@ -0,0 +1,34 @@
+//
+//  Enums.swift
+//  Memory Match
+//
+//  Created by Brendan on 2/19/24.
+//
+
+import Foundation
+
+enum GameThemeOptions: String, CaseIterable {
+    case defaultTheme = "Default"
+    case animals = "Animals"
+    case foods = "Foods"
+    case elements = "Elements"
+
+    private var allTokens: [String] {
+        switch self {
+        case .defaultTheme:
+            return ["🔥", "⭐️", "🎱", "❤️", "🤖", "🍔", "🚗", "💎"]
+        case .animals:
+            return ["🐠","🦆","🐢","🐪","🐖","🐈‍⬛","🐿️","🐍"]
+        case .foods:
+            return ["🍎","🍌","🍇","🫐","🍔","🍕","🥗","🍩"]
+        case .elements:
+            return ["🔥", "❄️", "💨", "💧", "🌱", "🌵", "⚡️", "🌸"]
+        }
+    }
+
+    var gameBoard: [GamePiece] {
+        let selectedEmojis = allTokens.shuffled().prefix(8)
+        let pairs = selectedEmojis + selectedEmojis
+        return pairs.shuffled().map {GamePiece(symbol: $0)}
+    }
+}
